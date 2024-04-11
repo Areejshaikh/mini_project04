972e81af5fff2f6f49b2a2ef9e84dd31956828cf
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-04-12 00:44:55 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-// Q5 Write a program that greets the user based
-// on the time of day. Display Good Morning, 
-// Good Afternoon, or Good Evening based on the 
-// time of day when you run the code.
-var timeOfDay = new Date();
-if (timeOfDay.getHours() < 12) {
-    console.log("Good Morning");
+// Q4 Create a program to check a student's grade and percentage.
+import inquirer from "inquirer";
+let user_Per = await inquirer.prompt({
+    message: "Enter your percentage",
+    name: "user_output",
+    type: "input"
+});
+if (user_Per.user_output < 50) { // conditions if user output less than 50, grade F
+    console.log("Your grade is 'F' !");
 }
-else if (timeOfDay.getHours() < 16) {
-    console.log("Good afternoon");
+else if (user_Per.user_output > 50 && user_Per.user_output < 60) {
+    console.log("Your grade is 'D' !");
 }
-else if (timeOfDay.getHours() < 18) {
-    console.log("Good Eving");
+else if (user_Per.user_output > 60 && user_Per.user_output < 70) {
+    console.log("Your grade is 'C' !");
 }
-else if (timeOfDay.getHours() < 24) {
-    console.log("Good Night");
+else if (user_Per.user_output > 70 && user_Per.user_output < 80) {
+    console.log("Your grade is 'B' !");
+}
+else if (user_Per.user_output > 80 && user_Per.user_output < 90) {
+    console.log("Your grade is 'A' !");
+}
+else if (user_Per.user_output > 90 && user_Per.user_output < 100) {
+    console.log("Your grade is 'A+' !");
+}
+else {
+    console.log("Invalid Percentage");
 }
--- index.ts
+++ index.ts
@@ -1,23 +1,32 @@
-// Q5 Write a program that greets the user based
-// on the time of day. Display Good Morning, 
-// Good Afternoon, or Good Evening based on the 
-// time of day when you run the code.
 
+// Q4 Create a program to check a student's grade and percentage.
 
+import inquirer from "inquirer";
 
-let timeOfDay = new Date();
-if (timeOfDay.getHours() <  12) {
-console.log("Good Morning")
-} else if(timeOfDay.getHours() < 16) {
-    console.log("Good afternoon")
-}
-else if(timeOfDay.getHours() < 18) {
-  console.log("Good Eving")
-}
-else if(timeOfDay.getHours() < 24) {
-  console.log("Good Night")
-}
+let user_Per : any  = await inquirer.prompt({ // user input
+    message :"Enter your percentage", 
+    name : "user_output", 
+    type: "input"});
 
-
-
-
+    if (user_Per.user_output < 50) { // conditions if user output less than 50, grade F
+        console.log("Your grade is 'F' !");
+    }
+    else if (user_Per.user_output > 50 && user_Per.user_output < 60){
+        
+        console.log("Your grade is 'D' !");
+    }
+    else if (user_Per.user_output > 60 && user_Per.user_output < 70){
+        console.log("Your grade is 'C' !");
+    }
+    else if (user_Per.user_output > 70 && user_Per.user_output < 80){
+        console.log("Your grade is 'B' !");
+    }
+    else if (user_Per.user_output > 80 && user_Per.user_output < 90){
+        console.log("Your grade is 'A' !");
+    }
+    else if (user_Per.user_output > 90 && user_Per.user_output < 100){
+        console.log("Your grade is 'A+' !");
+    }
+    else {
+        console.log("Invalid Percentage");
+    }
